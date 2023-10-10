86c75f131de3573619b8ba304267920aa7104d68
Will Bollock <wbollock@linode.com>
2023-10-10 10:35:26 -0400

    test: add root, metrics, probe integration tests

--- test/integration_test.go
+++ test/integration_test.go
@@ -1,25 +1,72 @@
 package integrationtest
 
 import (
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/wbollock/ping_exporter/internal/server"
 )
 
-func TestPingExporterEndpoint(t *testing.T) {
-	handler := server.SetupServer("/metrics")
-	server := httptest.NewServer(handler)
+const expectedStatusCode = 200
+
+func setupTestServer() *httptest.Server {
+	handler := server.SetupServer()
+	return httptest.NewServer(handler)
+}
+
+func validateResponse(t *testing.T, resp *http.Response, expectedBodyContent string) {
+	if resp.StatusCode != expectedStatusCode {
+		t.Fatalf("Expected status %d, got: %d", expectedStatusCode, resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("Failed to read body: %v", err)
+	}
+
+	if !strings.Contains(string(body), expectedBodyContent) {
+		t.Fatalf("Expected %s, got: %v", expectedBodyContent, string(body))
+	}
+}
+
+func TestPingExporterRootEndpoint(t *testing.T) {
+	server := setupTestServer()
 	defer server.Close()
 
-	resp, err := http.Get(server.URL + "/your_endpoint")
+	resp, err := http.Get(server.URL + "/")
 	if err != nil {
 		t.Fatalf("Failed to send GET request: %v", err)
 	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode != 200 {
-		t.Fatalf("Expected status 200, got: %d", resp.StatusCode)
+	validateResponse(t, resp, "<head><title>Ping Exporter</title></head>")
+}
+
+func TestPingExporterMetricsEndpoint(t *testing.T) {
+	server := setupTestServer()
+	defer server.Close()
+
+	resp, err := http.Get(server.URL + "/metrics")
+	if err != nil {
+		t.Fatalf("Failed to send GET request: %v", err)
 	}
+	defer resp.Body.Close()
+
+	validateResponse(t, resp, "promhttp_metric_handler_requests_in_flight 1")
+}
+
+func TestPingExporterProbeEndpoint(t *testing.T) {
+	server := setupTestServer()
+	defer server.Close()
+
+	resp, err := http.Get(server.URL + "/probe?target=127.0.0.1")
+	if err != nil {
+		t.Fatalf("Failed to send GET request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	validateResponse(t, resp, "ping_success 1")
 }
